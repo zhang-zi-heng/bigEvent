18a75b18fd978340c0121647af61017e816795d2
ziheng <1989093915@qq.com>
2020-09-03 14:44:03 +0800

    index over

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,71 @@
-$(function() {
+// 入口函数
+$(function () {
+    getUserInfo()
+    var layer = layui.layer //layer功能
+    // 退出功能点击事件
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers请求头
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // 渲染用户头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // 1.获取 昵称 或 用户名
+    var name = user.nickname || user.username
+    // 2.设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3.按需求渲染头像
+    if (user.user_pic !== null) {
+        // attr 设置并返回被选元素的属性值
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+
+
+
+
+
+
+
+
+
+/* $(function() {
   // 调用 getUserInfo 获取用户基本信息
   getUserInfo()
 
@@ -69,3 +136,4 @@ function renderAvatar(user) {
       .show()
   }
 }
+ */
